b6c94fc5417c70c37ea8ec3161ac2b18e978cc36
nickfsk <nicholas.foo@outlook.com.>
2025-12-08 09:45:03 +0800

    Form added

--- i_form/form/script.js
+++ i_form/form/script.js
@@ -2,21 +2,73 @@
 // Setting the Attributes of HTML select input
 
 // Create the array to populate selectDom
+const arrTopics = new Array("Methods", "Properies", "Event Listeners");
+
 
 // Create the array to store selected values (for submission)
-
+const arrSelected = new Array();
 // Declare constant selectDom to refer to <select> of id = "selectTopics"
+//Line 31 in the form.html
+const selectDom =  document.getElementById("selectTopics");
 
 // Set the attributes of selectDom
+selectDom.setAttribute("multiple", "true");
 
 // Populate the options in selectDom
+arrTopics.forEach((topic, index) => {
+selectDom.options.add(new Option(`${index+1}. ${topic}`, topic));
+});
 
 // 11. element.getAttribute()
 // get the Attributes's value(s) of selected options
-
+arrTopics.forEach(( topic, index) => {
+    selectDom.options[index].setAttribute("class", "btn btn-primary d-block m-2 fs-6");
+});
 // 12. element.addEventListener()
 // Adding an event listener to a button
+selectDom.addEventListener("change", () => {
+  
+    // get the collection of options from the <select> object
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/selectedOptions
+    const collection = selectDom.selectedOptions;
+    
+    // iterate through the collection
+    for (let index = 0; index < collection.length; index++) {
+        
+        // obtain the selected option from <select>
+        const itemValue = collection[index].getAttribute("value");
+        
+        // check it arrSelected contains one of the selected list times
+        const found = arrSelected.find((item) => item === itemValue);
 
+        // only if it is not found, then add it to the list to display
+        if(!found){
+            
+            // create the list item element to be appended on to listSelected <ul>
+            // list item element should show the text stored in itemValue
+            const listItem = document.createElement("li");
+            listItem.textContent = itemValue;
+            document.getElementById("listSelected").append(listItem);
+
+            // update arrSelected
+            arrSelected.push(itemValue);
+        }
+
+
+    }
+
+});
+
+//selectDom.addEventListener
 // Clear the selection from listSelected and arrSelected
 // Use the splice method to remove all elements in an array
+const btnClearSelectList = document.getElementById("btnClearSelection");
 
+btnClearSelectList.addEventListener("click", () =>{
+    document.getElementById("listSelected").innerHTML = "";
+    //Clear the array arrSelected
+    //console.log(arrSelected);
+    arrSelected.splice(0, arrSelected.length);
+    console.log(arrSelected.length);
+
+});
